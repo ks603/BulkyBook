3632c96b94dd70b3827b26779354fd870cef7b37
Kevin <kevin.shaughnessy18@gmail.com>
2022-07-28 17:00:17 -0400

    Created Category model

---
+++ BulkyWebApp/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyWebApp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
