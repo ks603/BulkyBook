07f2ceb4e38f0264dfdaaa5e629ec081a821b915
Kevin <kevin.shaughnessy18@gmail.com>
2022-07-28 20:41:37 -0400

    Imported Entity Framework to create the table

---
+++ BulkyWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options) : base(Options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
