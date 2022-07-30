06f4e79475ad645bb64066f5b6c2b266b9df60fd
Kevin <kevin.shaughnessy18@gmail.com>
2022-07-30 13:55:50 -0400

    Created variable that gets all categories

--- BulkyWebApp/Controllers/CategoryController.cs
+++ BulkyWebApp/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var objectCategoryList = _db.Categories.ToList();
             return View();
         }
     }
